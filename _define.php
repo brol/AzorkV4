4fce5570635ce4a91952c73597826e8bdaf2493e
brol <brol31@gmail.com>
2016-04-08 14:49:36 +0200

    v1.0
    changelog

---
+++ _define.php
@@ -0,0 +1,23 @@
+<?php
+# ***** BEGIN LICENSE BLOCK *****
+#
+#  	AzorkV4
+#  	Theme by Azork (http://xtradotfreedotfr.free.fr/blog/)
+#   Contributor : Pierre Van Glabeke
+#   Icones by Fam Fam Fam (www.famfamfam.com)
+#   License : public domain
+#
+# ***** END LICENSE BLOCK *****
+if (!defined('DC_RC_PATH')) { return; }
+
+$this->registerModule(
+	/* Name */			    "AzorkV4",
+	/* Description*/		"Adaptation du kit Aorakit 2C de Kozlika",
+	/* Author */			  "Azork, Pierre Van Glabeke",
+	/* Version */			  '1.0',
+	array(
+		'type'		=>		'theme',
+		'tplset' => 'mustek',
+		'dc_min' => '2.9'
+	)
+);
